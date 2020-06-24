e459aff56f040d4749d37f5a70d3e45b7e0c3082
Nicolas <nisoffi@gmail.com>
2020-06-24 16:38:17 -0300

    Primeira versão da Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,63 @@
 <template>
   <div class="home">
     <b-container>
-      <b-row class="color" align-v="center">
-        <b-col>1 of 3</b-col>
+      <b-row class="m-4" align-v="center" align-h="center">
+        <h1>Covid Monitor</h1>
+      </b-row>
+      <b-row class="mt-1 item-region" align-v="center">
+        <b-col class="item-flex">
+          <b-icon-arrow-right-square-fill></b-icon-arrow-right-square-fill>
+          <h6 class="item-flex">Centro-Oeste</h6>
+        </b-col>
+      </b-row>
+      <b-row class="mt-1 item-region" align-v="center">
+        <b-col class="item-flex">
+          <b-icon-arrow-right-square-fill></b-icon-arrow-right-square-fill>
+          <h6 class="item-flex">Sudoeste</h6>
+        </b-col>
+      </b-row>
+      <b-row class="mt-1 item-region" align-v="center">
+        <b-col class="item-flex">
+          <b-icon-arrow-right-square-fill></b-icon-arrow-right-square-fill>
+          <h6 class="item-flex">Norte</h6>
+        </b-col>
+      </b-row>
+      <b-row class="mt-1 item-region" align-v="center">
+        <b-col class="item-flex">
+          <b-icon-arrow-right-square-fill></b-icon-arrow-right-square-fill>
+          <h6 class="item-flex">Sul</h6>
+        </b-col>
+      </b-row>
+      <b-row class="mt-1 item-region" align-v="center">
+        <b-col class="item-flex">
+          <b-icon-arrow-right-square-fill></b-icon-arrow-right-square-fill>
+          <h6 class="item-flex">Nordeste</h6>
+        </b-col>
       </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
+import { BIconArrowRightSquareFill } from 'bootstrap-vue';
+
 export default {
   name: 'Home',
   components: {
+    BIconArrowRightSquareFill,
   },
 };
 </script>
 
 <style scoped>
-.color{
-  background-color: aqua;
+.item-region {
+  background-color: #DCDCDC;
+  border-radius: 4px;
+  height: 50px;
+}
+
+.item-flex{
+  display: flex;
+  padding: 0 10px;
 }
 </style>
