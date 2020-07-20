b04990ca6ccb8023f5def5fa62d90797952a634f
Nicolas <nisoffi@gmail.com>
2020-07-20 14:56:28 -0300

    removendo dist do gitignore

---
+++ dist/precache-manifest.3020239c95a11e8dfda0245d582c667b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468edf4d511a2cdf9e10",
+    "url": "/css/about.1ac93eb7.css"
+  },
+  {
+    "revision": "7eba66063d2aaedb3570",
+    "url": "/css/chunk-vendors.084c0951.css"
+  },
+  {
+    "revision": "4b9a8b98056ed0189fe86f24a6562012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "468edf4d511a2cdf9e10",
+    "url": "/js/about.5d884095.js"
+  },
+  {
+    "revision": "ae8e78f020421dcd4828",
+    "url": "/js/app.1fc29e12.js"
+  },
+  {
+    "revision": "7eba66063d2aaedb3570",
+    "url": "/js/chunk-vendors.16949f5e.js"
+  },
+  {
+    "revision": "d97cc3330526ee9795d558bc1b393c47",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
